98c4975767f326c0a4d02f39a4d975744ebe984a
saes1102 <43134939+saes1102@users.noreply.github.com>
2018-11-28 09:03:53 +0100

    Update Totientgenerator.py

--- Totientgenerator.py
+++ Totientgenerator.py
@@ -1,50 +1,38 @@
-# Denne funksjon genererer en pandas dataframe med tre kollonner og et gitt antall rader
-# der den forste kollonnen er naturlige tall n fra og med 1 til og med et gitt 
-# antall, den andre kollonnen er hvor mange tall mindre enn n som er innbyrdes 
-# primske med tallet n, og den tredje kollonen er en liste av alle faktorer av
-# n. Denne kan så gjores om til en .csv fil og analyseres
+"""
+Denne funksjonen lager en pandas dataframe med kolonner n, phy(n) og faktorer av n for alle naturlige tall fra 1 til "maxverdi"
+"""
 
-# Av Espen Sales
-# 26/11/2018
+def TotientGenerator(maxverdi):
+    from pandas import DataFrame
 
-import pandas as pd
+    nliste = [1]
+    phyliste = [1]
+    faktorliste = [[1]]
 
-maxverdi = 1000
+    for x in range(2,maxverdi+1):
+        n = x
+        phy = 0
+        faktorer = []
+        for y in range(1,x+1):
+            if x%y == 0:
+                faktorer.append(y)
 
-nliste = [1]
-phyliste = [1]
-faktorliste = [[1]]
+        for y in faktorliste:
+            innbyrdes = True
+            for z in range(1,len(faktorer)):
+                if faktorer[z] in y:
+                    innbyrdes = False
+            if innbyrdes == True:
+                phy += 1
 
-for x in range(2,maxverdi+1):
-    n = x
-    phy = 0
-    faktorer = []
-    for y in range(1,x+1):
-        if x%y == 0:
-            faktorer.append(y)
-    
-    for y in faktorliste:
-        innbyrdes = True
-        for z in range(1,len(faktorer)):
-            if faktorer[z] in y:
-                innbyrdes = False
-        if innbyrdes == True:
-            phy += 1
-    
-    nliste.append(n)
-    phyliste.append(phy)
-    faktorliste.append(faktorer)
+        nliste.append(n)
+        phyliste.append(phy)
+        faktorliste.append(faktorer)
 
-tabell = []
-for x in range(0,maxverdi):
-    rad = [nliste[x],phyliste[x],faktorliste[x]]
-    tabell.append(rad)
-
-tabell.insert(0,[0,0,[0]])
-    
-#for x in tabell:
-#    print(x)
-
-kolonner = ["n","phy(n)","faktorer"]
-
-dataframe = pd.DataFrame(tabell, columns = kolonner)
+    tabell = []
+    for x in range(0,maxverdi):
+        rad = [nliste[x],phyliste[x],faktorliste[x]]
+        tabell.append(rad)
+    kolonner = ["n","phy(n)","faktorer"]
+    dataframe = pd.DataFrame(tabell, columns = kolonner)
+    return dataframe
