6ca34e0f4259d11f4e45a6bb0fb4ee10bf45f19e
saes1102 <43134939+saes1102@users.noreply.github.com>
2018-11-29 14:52:21 +0100

    Update Totientgenerator.py

--- Skriving av csv fil/Totientgenerator.py
+++ Skriving av csv fil/Totientgenerator.py
@@ -1,23 +1,19 @@
-"""
-Denne funksjonen lager en pandas dataframe med kolonner n, phy(n) og faktorer av n for alle naturlige tall fra 1 til "maxverdi"
-"""
-
-def TotientGenerator(maxverdi):
+def TotientGenerator(maxverdi):     # "maxverdi" er hvor mange rader .csv filen inneholder, altså hvor mange naturlige tall fra 1 til "maxverdi" som inkluderes
     from pandas import DataFrame
 
-    nliste = [1]
+    nliste = [1]      # De tre kolonnene starter som lister, og inneholder verdiene for n=1
     phyliste = [1]
     faktorliste = [[1]]
 
-    for x in range(2,maxverdi+1):
+    for x in range(2,maxverdi+1):    # Fra n=2 til og med "maxverdi" blir kolonnene forlenget 
         n = x
         phy = 0
         faktorer = []
-        for y in range(1,x+1):
+        for y in range(1,x+1):    # Faktorene til n beregnes og legges til i listen av faktorer
             if x%y == 0:
                 faktorer.append(y)
 
-        for y in faktorliste:
+        for y in faktorliste:    # Med brute force sjekker programmet hvor mange tall mindre enn n som er innbyrdes primske med n
             innbyrdes = True
             for z in range(1,len(faktorer)):
                 if faktorer[z] in y:
@@ -25,14 +21,17 @@ def TotientGenerator(maxverdi):
             if innbyrdes == True:
                 phy += 1
 
-        nliste.append(n)
+        nliste.append(n)        # Verdiene legges til sine respektive kolonner
         phyliste.append(phy)
         faktorliste.append(faktorer)
 
-    tabell = []
+    tabell = []                    # Den endelige tabellen ferdigstilles 
     for x in range(0,maxverdi):
         rad = [nliste[x],phyliste[x],faktorliste[x]]
         tabell.append(rad)
+        
     kolonner = ["n","phy(n)","faktorer"]
+    filnavn = "Totient"+str(maxverdi)+"verdier.csv"         # "filnavn" er skrevet slik at navnet på .csv filen reflekterer hvor mange verdier av n er inkludert
     dataframe = pd.DataFrame(tabell, columns = kolonner)
-    return dataframe
+    
+    dataframe.to_csv(filnavn)    # .csv filen lagres i samme mappe som denne jupyter filen, funksjonen behøver ikke returnere noe
