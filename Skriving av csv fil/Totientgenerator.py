91d8973462fcf6a240687ab6bde9778b2ad97704
saes1102 <43134939+saes1102@users.noreply.github.com>
2018-12-10 13:52:42 +0100

    Update Totientgenerator.py

--- Skriving av csv fil/Totientgenerator.py
+++ Skriving av csv fil/Totientgenerator.py
@@ -1,37 +1,47 @@
-def TotientGenerator(maxverdi):     # "maxverdi" er hvor mange rader .csv filen inneholder, altså hvor mange naturlige tall fra 1 til "maxverdi" som inkluderes
+def TotientGenerator(maxverdi):
     from pandas import DataFrame
-
-    nliste = [1]      # De tre kolonnene starter som lister, og inneholder verdiene for n=1
+    
+    nliste = [1]
     phyliste = [1]
-    faktorliste = [[1]]
-
-    for x in range(2,maxverdi+1):    # Fra n=2 til og med "maxverdi" blir kolonnene forlenget 
+    divisorlister = [[1]]
+    divisorstrenger = ["1"]
+    tabell = []
+    
+    for x in range(2,maxverdi+1):
         n = x
         phy = 0
-        faktorer = []
-        for y in range(1,x+1):    # Faktorene til n beregnes og legges til i listen av faktorer
-            if x%y == 0:
-                faktorer.append(y)
-
-        for y in faktorliste:    # Med brute force sjekker programmet hvor mange tall mindre enn n som er innbyrdes primske med n
+        divisorer = []
+        divisorstreng = ""
+        
+        for y in range(1,n+1):
+            if n%y == 0:
+                divisorer.append(y)
+        
+        for y in divisorer:
+            divisorstreng = divisorstreng+str(y)+","
+        divisorstreng = divisorstreng[0:len(divisorstreng)-1]
+        
+        for y in divisorlister:    # y er divisorlistene
             innbyrdes = True
-            for z in range(1,len(faktorer)):
-                if faktorer[z] in y:
+            for z in divisorer:    # z er aktuelle divisorer
+                if z != 1 and z in y:    # hvis z ikke er 1 og finnes i andre divisorlister
                     innbyrdes = False
             if innbyrdes == True:
                 phy += 1
-
-        nliste.append(n)        # Verdiene legges til sine respektive kolonner
-        phyliste.append(phy)
-        faktorliste.append(faktorer)
-
-    tabell = []                    # Den endelige tabellen ferdigstilles 
-    for x in range(0,maxverdi):
-        rad = [nliste[x],phyliste[x],faktorliste[x]]
-        tabell.append(rad)
         
-    kolonner = ["n","phy(n)","faktorer"]
-    filnavn = "Totient"+str(maxverdi)+"verdier.csv"         # "filnavn" er skrevet slik at navnet på .csv filen reflekterer hvor mange verdier av n er inkludert
-    dataframe = pd.DataFrame(tabell, columns = kolonner)
+        nliste.append(n)
+        phyliste.append(phy)
+        divisorlister.append(divisorer)
+        divisorstrenger.append(divisorstreng)
+        
+    tabell = []
     
-    dataframe.to_csv(filnavn)    # .csv filen lagres i samme mappe som denne jupyter filen, funksjonen behøver ikke returnere noe
+    for x in range(0,maxverdi):
+        rad = [nliste[x],phyliste[x],divisorstrenger[x]]
+        tabell.append(rad)
+    
+    kolonner = ["n","phy(n)","divisorer"]
+    filnavn = "totient"+str(maxverdi)+"n.csv"
+    dataframe = DataFrame(tabell,columns = kolonner)
+    dataframe.to_csv(filnavn,index=False)
+    
